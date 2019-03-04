9be74563a93f365f7d0f0becc76c47db469ae0c5
ezraith <oscardyh@gmail.com>
2019-03-04 16:17:51 +0800

    Add drawing arc road type

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from roadgeometry import RoadGeometry, RoadLine
+from roadgeometry import RoadLine, RoadArc
 import matplotlib.pyplot as plt
 import math
 
-roadl = RoadLine(0, -7.0710678117841717e+00, 7.0710678119660715e+00, 5.4977871437752235e+00, 4.8660000002386400e-01)
+road1 = RoadLine(0, 7.0710678120487289e+00, 7.0710678117015151e+00, 3.9269908169686372e+00, 4.8660000002387466e-01)
+road5 = RoadLine(1.6031224248136859e+01, -6.7269902520731444e+00, 6.7269896521996131e+00, 2.3561944901771223e+00, 4.8660000002378739e-01)
 
-roadl.graph()
+road3 = RoadArc(3.6612031746270493e+00,
+                4.3409250448908985e+00,
+                4.6416930097877751e+00,
+                3.7254287106480075e+00,
+                9.1954178989066371e+00,
+                -1.2698412698412698e-01)
 
+# roada = RoadArc(0, 0, 1, math.pi, 28, -1.2698412698412698e-01)
+
+road1.graph()
+road5.graph()
+road3.graph()
+
+plt.xlim(-10, 10)
+plt.ylim(0, 20)
 plt.show()
--- roadgeometry.py
+++ roadgeometry.py
@@ -28,9 +28,37 @@ class RoadArc(RoadGeometry):
         self.curvature = curvature
 
     def graph(self):
-        x = np.array(range(0, self.length))
+        r, x, y, array = self.generate_coords()
+        print(r, x, y)
+        for c in range(len(array)):
+            print(array[c])
+        xarr = np.array([r * math.cos(x) for x in array])
 
-    def c
+        yarr = np.array([r * math.sin(y) for y in array])
+        print(xarr, yarr)
+        plt.plot(x + xarr, y + yarr, 'b-')
+
+    def generate_coords(self):
+        radius = math.fabs(1/self.curvature)
+        print(radius)
+        circumference = radius * math.pi * 2
+        angle = (self.length/circumference) * 2 * math.pi
+        print(angle)
+        # if clockwise, then curvature < 0
+        if self.curvature > 0:
+            #TODO Test the anticlockwise case for drawArc
+            start_angle = self.hdg - (math.pi / 2)
+            circlex = self.x + (math.cos(start_angle) * radius)
+            circley = self.y + (math.sin(start_angle) * radius)
+
+            array = list(range(61))
+            return radius, circlex, circley, [start_angle + (angle * x / 60) for x in array]
+        else:
+            start_angle = self.hdg + (math.pi / 2)
+            circlex = self.x - (math.cos(start_angle) * radius)
+            circley = self.y - (math.sin(start_angle) * radius)
+            array = list(range(61))
+            return radius, circlex, circley, [start_angle - (angle * x / 60) for x in array]
 
 
 class RoadSpiral(RoadGeometry):
@@ -60,16 +88,12 @@ class RoadParamPoly3(RoadGeometry):
 
 
 def nprange(end):
-    if end < 5:
-        array = list(range(math.floor(end*10)))
-        array = [x/10 for x in array]
-        if math.floor(end) != end:
-            array.append(end)
+    if end < 11:
+        array = list(range(11))
+        array = [end*x/10 for x in array]
     else:
         array = list(range(math.floor(end)))
         if math.floor(end) != end:
             array.append(end)
-    for x in range(len(array)):
-        print(array[x])
     return np.array(array)
 
