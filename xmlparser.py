5c8d20bc99fe453f461d52d403072d4a595a958b
ezraith <oscardyh@gmail.com>
2019-03-07 10:59:18 +0800

    Add parser for junctions

--- xmlparser.py
+++ xmlparser.py
@@ -1,25 +1,29 @@
 from lxml import etree
 from road import Road
 from roadgeometry import RoadGeometry, RoadLine, RoadSpiral, RoadArc
+from junction import Junction, Connection
 from matplotlib import pyplot as plt
 
 class XMLParser(object):
     def __init__(self, file):
         self.xml = etree.parse(file)
-        self.opendrive = xml.getroot()
-        self.header = opendrive.find('header')
+        self.root = self.xml.getroot()
+        self.opendrive = OpenDrive()
+        self.header = self.root.find('header')
 
 
+    # Parses all roads in the xodr and instantiates them into objects
+    # Returns a list of Road objects
     def parse_roads(self):
         ret = list()
 
-        for road in opendrive.iter('road'):
+        for road in self.root.iter('road'):
 
+            # Create the Road object
             new_road = Road(road.get('name'), road.get('length'), road.get('id'), road.get('junction'))
 
             plan_view = road.find('planView')
-
-            for geometry in road.iter('geometry'):
+            for geometry in plan_view.iter('geometry'):
                 record = geometry[0].tag
 
                 s = float(geometry.get('s'))
@@ -29,32 +33,35 @@ class XMLParser(object):
                 length = float(geometry.get('length'))
 
                 if record == 'line':
-                    new_road.add_plan(RoadLine(s, x, y, hdg, length))
+                    new_road.add_record(RoadLine(s, x, y, hdg, length))
                 elif record == 'arc':
                     curvature = float(geometry[0].get('curvature'))
-                    new_road.add_plan(RoadArc(s, x, y, hdg, length, curvature))
+                    new_road.add_record(RoadArc(s, x, y, hdg, length, curvature))
                 elif record == 'spiral':
                     curv_start = float(geometry[0].get('curvStart'))
                     curv_end = float(geometry[0].get('curvEnd'))
-                    new_road.add_plan(RoadSpiral(s, x, y, hdg, length, curv_start, curv_end))
+                    new_road.add_record(RoadSpiral(s, x, y, hdg, length, curv_start, curv_end))
 
             ret.append(new_road)
 
         return ret
 
-# Crossing8Course.xodr
-xml = etree.parse("Crossing8Course.xodr")
-print(etree.tostring(xml))
+    # TODO Add Priorities, JunctionGroups and LaneLinks
+    def parse_junctions(self):
+        ret = list()
 
-opendrive = xml.getroot()
-print(opendrive.tag)
-header = opendrive.find('header')
-print(header.tag)
+        for junction in self.root.iter('junction'):
+            new_junction = Junction(junction.get('name'), junction.get('id'))
 
-parser = XMLParser('test.xodr')
-roads = parser.parse_roads()
-for road in roads:
-    road.draw_road()
-plt.show()
+            for connection in junction.iter('connection'):
+                id = connection.get('id')
+                incomingRoad = connection.get('incomingRoad')
+                connectingRoad = connection.get('connectingRoad')
+                contactPoint = connection.get('contactPoint')
+                new_connection = Connection(id, incomingRoad, connectingRoad, contactPoint)
 
+                new_junction.add_connection(new_connection)
 
+            ret.append(new_junction)
+
+        return ret
