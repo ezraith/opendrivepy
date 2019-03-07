bfb361b97e42f12f17bb9851503e1c3fad74d0cd
ezraith <oscardyh@gmail.com>
2019-03-07 10:58:26 +0800

    Add Junction and Connection object

---
+++ junction.py
@@ -0,0 +1,23 @@
+
+
+class Junction(object):
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.connections = list()
+        self.priorities = list()
+        self.controllers = list()
+
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+class Connection(object):
+    def __init__(self, id, incoming_road, connecting_road, contact_point):
+        self.id = id
+        self.incoming_road = incoming_road
+        self.connecting_road = connecting_road
+        self.contact_point = contact_point
+        self.lane_link = list()
+
+# TODO Add Priorities, JunctionGroups and LaneLinks
+
